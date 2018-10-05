a327342ef40442e04067a47b206598a2432f68a1
Higher Stark <higherwashington@gmail.com>
2018-10-05 20:35:30 +0800

    pass lab2 part2-a

--- lock_server.cc
+++ lock_server.cc
@@ -9,6 +9,8 @@
 lock_server::lock_server():
   nacquire (0)
 {
+  pthread_mutex_init(&mutex, NULL);
+  pthread_cond_init(&cond, NULL);
 }
 
 lock_protocol::status
@@ -25,6 +27,13 @@ lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
 {
   lock_protocol::status ret = lock_protocol::OK;
 	// Your lab2 part2 code goes here
+  printf("acquire request from clt %d\n", clt);
+  pthread_mutex_lock(&mutex);
+  while (locks.find(lid) != locks.end() && locks[lid]) {
+    pthread_cond_wait(&cond, &mutex);
+  }
+  locks[lid] = true;
+  pthread_mutex_unlock(&mutex);
   return ret;
 }
 
@@ -33,5 +42,10 @@ lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
 {
   lock_protocol::status ret = lock_protocol::OK;
 	// Your lab2 part2 code goes here
+  printf("release request from clt %d\n", clt);
+  pthread_mutex_lock(&mutex);
+  locks[lid] = false;
+  pthread_cond_signal(&cond);
+  pthread_mutex_unlock(&mutex);
   return ret;
 }
